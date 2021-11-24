04af8cc8ef936f82f84245e4bed48812fa4caa3d
umairtk <94683648+umairtk@users.noreply.github.com>
2021-11-24 12:43:12 +0500

    Change
    Change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World WatchTower');
+  res.send('Hello World WatchTower Aakaash');
 });
 
 app.listen(PORT, HOST);
